283ac6078c3805e55d92aa3f8d2dae7ec9e003cd
Alison Janise Paico Quispe <alison.jpq@gmail.com>
2024-10-28 21:40:43 -0500

    agrege funcionalidad a lupa buscador, lleva a pag mantenimiento

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
 import './SearchBar.css';
+import { Link } from "react-router-dom";
 import { IoSearch } from "react-icons/io5";
 
 const SearchBar = () => {
-    
+
     return (
-    <>
-        <div className='search-bar'>
-            <input
-            type="text"
-            placeholder="Que estás buscando?"
-            />
-            <IoSearch size={22} style={{ paddingLeft: "2%" }} />
-        </div>
-    </>
+        <>
+            <div className='search-bar'>
+                <input
+                    type="text"
+                    placeholder="Que estás buscando?"
+                />
+                <Link to="/wip">
+                    <IoSearch size={22} style={{ paddingLeft: "2%" }} />
+                </Link>
+            </div>
+        </>
     )
 }
 
