ab5472169dc1be81ffdc5bb69200f9d642567d58
Alison Janise Paico Quispe <alison.jpq@gmail.com>
2024-10-30 12:32:41 -0500

    back

--- src/Routes/ListaProductos.jsx
+++ src/Routes/ListaProductos.jsx
@@ -1,59 +1,66 @@
-import React from 'react'
 import StylesListaProductos from '../Styles/ListaProductos.module.css'
 import CardProductos from '../Components/CardProductos'
-import { useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-function ListaProductos({listaProductos}) {
+function ListaProductos({ listaProductos }) {
 
     const params = useParams();
     //filtramos los que sean de esa categoria
-    const productosFiltrados = listaProductos.filter(producto => producto.categoria.idCategoria == params.id);  
+    const productosFiltrados = listaProductos.filter(producto => producto.categoria.idCategoria == params.id);
     // Copia del array original
-    let datosProductos= [...productosFiltrados];
+    let datosProductos = [...productosFiltrados];
     let productosAleatorias = [];
 
-function mostrarElementoAleatorioSinRepetir(datosRestantes) {
-    if (datosRestantes.length === 0) {
-        console.log("Ya se mostraron todos los elementos.");
-        return;
+    function mostrarElementoAleatorioSinRepetir(datosRestantes) {
+        if (datosRestantes.length === 0) {
+            console.log("Ya se mostraron todos los elementos.");
+            return;
+        }
+        // Elegir un índice aleatorio
+        const indiceAleatorio = Math.floor(Math.random() * datosRestantes.length);
+
+        // Obtener y eliminar el elemento aleatorio
+        const elementoAleatorio = datosRestantes.splice(indiceAleatorio, 1)[0];
+        return elementoAleatorio;
     }
-    // Elegir un índice aleatorio
-    const indiceAleatorio = Math.floor(Math.random() * datosRestantes.length);
 
-    // Obtener y eliminar el elemento aleatorio
-    const elementoAleatorio = datosRestantes.splice(indiceAleatorio, 1)[0];
-    return elementoAleatorio;
-}
-
-for (let i = 0; i < productosFiltrados.length; i++) {
-    let producto=mostrarElementoAleatorioSinRepetir(datosProductos);
-    productosAleatorias.push(producto);
-}
-
-
-const obtenerTituloCategoria = ()=>{
-    let titulo = 'Vajillas';
-    if(params.id==2){
-        titulo='Cubiertos';
+    for (let i = 0; i < productosFiltrados.length; i++) {
+        let producto = mostrarElementoAleatorioSinRepetir(datosProductos);
+        productosAleatorias.push(producto);
     }
-    else if(params.id==3){
-        titulo='Cristaleria';
+
+
+    const obtenerTituloCategoria = () => {
+        let titulo = 'Vajillas';
+        if (params.id == 2) {
+            titulo = 'Cubiertos';
+        }
+        else if (params.id == 3) {
+            titulo = 'Cristaleria';
+        }
+        return titulo;
     }
-    return titulo;
-}
 
-const titulo=obtenerTituloCategoria();
+    const titulo = obtenerTituloCategoria();
 
-  return (
-    <section className={StylesListaProductos.productos}>
-           <h3 className={StylesListaProductos.titulo}>{titulo}</h3>
-           <div className={StylesListaProductos.cardGrid}>
-           {productosAleatorias.map((producto)=>(
-                <CardProductos key={producto.id} producto={producto}/>
-            ))}
+    const navigate = useNavigate();
+
+    const onBack = () => {
+        navigate(-1)
+    }
+
+    return (
+        <section className={StylesListaProductos.productos}>
+            <h3 className={StylesListaProductos.titulo}>{titulo}</h3>
+            <div className={StylesListaProductos.cardGrid}>
+                {productosAleatorias.map((producto) => (
+                    <CardProductos key={producto.id} producto={producto} />
+                ))}
             </div>
-    </section>
-  )
+            <button className='btn-back' onClick={onBack}>Volver</button>
+        </section>
+    )
 }
 
 export default ListaProductos
