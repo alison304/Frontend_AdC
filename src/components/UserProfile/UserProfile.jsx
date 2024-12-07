c1721da970b6abf9274e08f4dea3d28bf8e2d6a8
Alison Janise Paico Quispe <alison.jpq@gmail.com>
2024-12-06 22:55:11 -0500

    se comento campo contraseña y boton guardar

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -10,10 +10,8 @@ const UserProfile = ({ onLogout }) => {
         nombre: localStorage.getItem('userNombre'),
         apellido: localStorage.getItem('userApellido'),
         email: localStorage.getItem('userEmail'),
-        password: '***',
+        // password: '***',
     });
-
-
 
     const handleTabChange = (tab) => {
         setActiveTab(tab);
@@ -30,10 +28,10 @@ const UserProfile = ({ onLogout }) => {
         navigate('/'); // Redirigir a la página de inicio
     };
 
-    const handleSaveChanges = () => {
-        // Lógica para guardar cambios
-        console.log('Datos guardados:', userData);
-    };
+    // const handleSaveChanges = () => {
+    //     // Lógica para guardar cambios
+    //     console.log('Datos guardados:', userData);
+    // };
 
     return (
         <div className="user-profile-container">
@@ -46,12 +44,12 @@ const UserProfile = ({ onLogout }) => {
                         Datos Personales
                     </button>
                 </div>
-
                 <div className="user-profile-content">
                     {activeTab === 'personal-data' && (
                         <div id="personal-data" className="user-profile-tab-content active">
                             <h2>Datos Personales</h2>
                             <div className="user-profile-form-group">
+                                <br/>
                                 <label htmlFor="nombre">Nombre</label>
                                 <input
                                     type="text"
@@ -59,6 +57,7 @@ const UserProfile = ({ onLogout }) => {
                                     value={userData.nombre}
                                     onChange={handleInputChange}
                                     placeholder="Ingresa tu nombre"
+                                    disabled
                                 />
                             </div>
                             <div className="user-profile-form-group">
@@ -69,6 +68,7 @@ const UserProfile = ({ onLogout }) => {
                                     value={userData.apellido}
                                     onChange={handleInputChange}
                                     placeholder="Ingresa tu apellido"
+                                    disabled
                                 />
                             </div>
                             <div className="user-profile-form-group">
@@ -82,7 +82,7 @@ const UserProfile = ({ onLogout }) => {
                                     disabled
                                 />
                             </div>
-                            <div className="user-profile-form-group">
+                            {/* <div className="user-profile-form-group">
                                 <label htmlFor="password">Contraseña</label>
                                 <input
                                     type="password"
@@ -94,7 +94,7 @@ const UserProfile = ({ onLogout }) => {
                             </div>
                             <button className="user-profile-button" onClick={handleSaveChanges}>
                                 Guardar Cambios
-                            </button>
+                            </button> */}
                         </div>
                     )}
                 </div>
